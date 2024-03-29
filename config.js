02047c78fccf8aad65888f53297b290b95ad4a17
godakanda59 <139145269+godakanda59@users.noreply.github.com>
2024-03-29 16:48:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 if (fs.existsSync("config.env"))
   require("dotenv").config({ path: "./config.env" });
